65d0b19ee0395a9f26f72b44a2dda136a39f2da8
haril song <songkg7@gmail.com>
2025-09-22 14:54:19 +0900

    fix: change a homerow non-search shortcut

--- modules/darwin/programs/homerow/default.nix
+++ modules/darwin/programs/homerow/default.nix
@@ -11,7 +11,7 @@
       check-for-updates-automatically = false;
       dash-speed-multiplier = 2;
       label-font-size = 10;
-      non-search-shortcut = "^⇧J";
+      non-search-shortcut = "⌥F";
       scroll-px-per-ms = 1.5;
       search-shortcut = "⌥⌘↩"; # ⌥⌘↩
     };
