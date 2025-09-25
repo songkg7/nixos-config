0104148a14a1766417542e803a423dff5927016f
haril song <songkg7@gmail.com>
2025-09-25 12:16:59 +0900

    Add AstroNvim template configuration

--- modules/darwin/home.nix
+++ modules/darwin/home.nix
@@ -79,8 +79,9 @@
     # };
 
     imports = [
-      inputs.nixvim.homeModules.nixvim
-      ../shared/programs/vim
+      # inputs.nixvim.homeModules.nixvim
+      # ../shared/programs/vim
+      ../shared/programs/astronvim
 
       # ../shared/programs/1password
       # ../shared/programs/act
