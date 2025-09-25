065da1a843b7e257dd7e362b17117edcc869cf39
haril song <songkg7@gmail.com>
2025-09-25 15:39:53 +0900

    Simplify Darwin module: only handle 1Password system installation, let default linkapps handle other apps

--- libraries/home-manager/systems/darwin/default.nix
+++ libraries/home-manager/systems/darwin/default.nix
@@ -5,39 +5,12 @@
     pathsToLink = "/Applications";
   };
 in {
-  # Enable spotlight application
-  home.activation.copyApplications = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    BASE_DIR="${config.home.homeDirectory}/Applications/Home Manager Apps"
-    mkdir -p "$BASE_DIR"
-
-    declare -a applications
-
-    for app_dir in "$BASE_DIR/"*; do
-      applications+=("$(basename "$app_dir")")
-    done
-
-    for app_dir in "${apps}/Applications/"*; do
-      applications+=("$(basename "$app_dir")")
-    done
-
-    for ((i = 0; i < ''${#applications[@]}; i++)); do
-      app="''${applications[$i]}"
-      rm -rf "$BASE_DIR/$app"
-
-      if [ -d "${apps}/Applications/$app" ]; then
-        if [[ "$app" = "1Password.app" ]]; then
-          # 1Password는 시스템 Applications에 설치
-          rm -rf "/Applications/$app"
-          $DRY_RUN_CMD cp ''$VERBOSE_ARG -fHRL "${apps}/Applications/$app" "/Applications"
-          $DRY_RUN_CMD chmod ''$VERBOSE_ARG -R +w "/Applications/$app"
-        else
-          # 다른 앱들은 사용자 Applications에 설치
-          $DRY_RUN_CMD cp ''$VERBOSE_ARG -fHRL "${apps}/Applications/$app" "$BASE_DIR"
-          $DRY_RUN_CMD chmod ''$VERBOSE_ARG -R +w "$BASE_DIR/$app"
-        fi
-      fi
-    done
+  # 1Password만 시스템 Applications에 특별 설치
+  home.activation.copy1Password = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    if [ -d "${apps}/Applications/1Password.app" ]; then
+      rm -rf "/Applications/1Password.app"
+      $DRY_RUN_CMD cp ''$VERBOSE_ARG -fHRL "${apps}/Applications/1Password.app" "/Applications"
+      $DRY_RUN_CMD chmod ''$VERBOSE_ARG -R +w "/Applications/1Password.app"
+    fi
   '';
-
-  # disabledModules = ["targets/darwin/linkapps.nix"];  # 기본 linkapps 모듈 사용
 }
