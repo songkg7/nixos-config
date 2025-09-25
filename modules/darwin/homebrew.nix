4750cabec4b6ee56438f765b7f459ba7ee18fb1c
haril song <songkg7@gmail.com>
2025-09-25 17:52:16 +0900

    add ssh config

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -1,11 +1,4 @@
-{
-  config,
-  lib,
-  ...
-}: let
-  inherit (lib) mkIf;
-  caskPresent = cask: lib.any (x: x.name == cask) config.homebrew.casks;
-in {
+{...}: {
   homebrew.enable = true;
   homebrew.onActivation.autoUpdate = true;
   homebrew.onActivation.cleanup = "uninstall";
@@ -28,18 +21,4 @@ in {
   homebrew.masApps = [
     # TODO: Amphetamine (ID: 937984704)
   ];
-
-  # Configuration related to casks
-  home-manager.users.${config.users.primaryUser.username} =
-    mkIf (caskPresent "1password" && config ? home-manager)
-    {
-      # https://developer.1password.com/docs/ssh/get-started
-      programs.ssh.enable = true;
-      programs.ssh.extraConfig = ''
-        # Only set `IdentityAgent` not connected remotely via SSH.
-        # This allows using agent forwarding when connecting remotely.
-        Match host * exec "test -z $SSH_TTY"
-          IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-      '';
-    };
 }
