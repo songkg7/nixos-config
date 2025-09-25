f6ca32f84cadd95bb6475ffa167bc463d0106f45
haril song <songkg7@gmail.com>
2025-09-25 16:52:25 +0900

    Revert "Switch 1Password to Homebrew management: enable homebrew module and configure 1password cask"
    This reverts commit f04e83f91297fb7626337e51872dfc5877fa2078.

--- libraries/home-manager/systems/darwin/default.nix
+++ libraries/home-manager/systems/darwin/default.nix
@@ -1,4 +1,16 @@
-{ config, pkgs, lib, ... }: {
-  # 1Password를 Homebrew로 관리
-  homebrew.casks = [ "1password" ];
+{ config, pkgs, lib, ... }: let
+  apps = pkgs.buildEnv {
+    name = "home-manager-applications";
+    paths = config.home.packages;
+    pathsToLink = "/Applications";
+  };
+in {
+  # 1Password만 시스템 Applications에 특별 설치
+  home.activation.copy1Password = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    if [ -d "${apps}/Applications/1Password.app" ]; then
+      rm -rf "/Applications/1Password.app"
+      $DRY_RUN_CMD cp ''$VERBOSE_ARG -fHRL "${apps}/Applications/1Password.app" "/Applications"
+      $DRY_RUN_CMD chmod ''$VERBOSE_ARG -R +w "/Applications/1Password.app"
+    fi
+  '';
 }
