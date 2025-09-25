a94869d9fe8e247b4e9266abbcedf2ea987e66b7
haril song <songkg7@gmail.com>
2025-09-25 14:20:33 +0900

    .

--- libraries/home-manager/programs/1password/default.nix
+++ libraries/home-manager/programs/1password/default.nix
@@ -4,7 +4,7 @@
   lib,
   ...
 }: let
-  cfg = config.programs._1password;
+  cfg = config.programs._1password-cli;
 
   makeWrapperFHSEnvironment = pkgs.runCommand "wrap-fhs-environment.sh" {} ''
     substitute ${./wrap-fhs-environment.sh} $out \
@@ -28,7 +28,7 @@
       })
     else cfg.package;
 in {
-  options.programs._1password = {
+  options.programs._1password-cli = {
     enable = lib.mkEnableOption "1password";
 
     package = lib.mkPackageOption pkgs "_1password" {};
