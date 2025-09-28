c3c5999e7f9b6a86629c8f9374a980aaaa87d499
kyungkeun.song <haril@kyungkeunsongs-MacBook-Pro.local>
2025-09-28 20:00:43 +0900

    .

--- modules/darwin/configuration.nix
+++ modules/darwin/configuration.nix
@@ -3,29 +3,6 @@
     ./programs/homebrew
   ];
 
-  system.defaults.dock = {
-    autohide = true;
-    tilesize = 40;
-    show-recents = false;
-    persistent-apps = [
-      "/System/Applications/Calendar.app"
-      "/System/Applications/Music.app"
-      "/Applications/Slack.app"
-      "/Applications/Telegram.app"
-      "/Applications/Warp.app"
-      "/Applications/KakaoTalk.app"
-    ];
-    persistent-others = [
-      {
-        path = "/Users/a14530/Downloads";
-        options = {
-          "show-as" = "fan";
-          "display-as" = "stack";
-        };
-      }
-    ];
-  };
-
   # Disable nix-darwin's Nix management to work with Determinate
   security.pam = {
     services.sudo_local.touchIdAuth = true;
