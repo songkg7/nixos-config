991991e104b0455ed84ca85fe0b69972439a7630
haril song <songkg7@gmail.com>
2025-09-29 12:20:27 +0900

    add colima ssh config

--- modules/darwin/programs/ssh/default.nix
+++ modules/darwin/programs/ssh/default.nix
@@ -2,7 +2,10 @@ _: {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    includes = ["~/.orbstack/ssh/config"];
+    includes = [
+      "~/.orbstack/ssh/config"
+      "~/.colima/ssh_config"
+    ];
     matchBlocks = {
       "*" = {
         identityAgent = "~/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
