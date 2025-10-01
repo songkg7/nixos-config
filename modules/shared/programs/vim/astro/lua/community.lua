c1ffffd82e82f52530cd7ce0e110ab0420e3fbce
haril song <songkg7@gmail.com>
2025-10-01 23:50:36 +0900

    add astro colorschemas

--- modules/shared/programs/vim/astro/lua/community.lua
+++ modules/shared/programs/vim/astro/lua/community.lua
@@ -8,8 +8,18 @@ return {
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  -- Colorscheme
   { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
   { import = "astrocommunity.colorscheme.rose-pine" },
   { import = "astrocommunity.colorscheme.oxocarbon-nvim" },
   { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.cyberdream-nvim" },
+
+  -- Motion
+  { import = "astrocommunity.motion.leap-nvim" },
+
+  -- Git
+  { import = "astrocommunity.git.git-blame-nvim" },
 }
