7d2eee865fa51e713ae8109c2229b2d10b304c03
haril song <songkg7@gmail.com>
2025-10-03 03:24:45 +0900

    Revert "update agenix config"
    This reverts commit 2e0085db126a03bc1a0a6605b5decd26223c955f.

---
+++ modules/shared/programs/age/default.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+let
+  inherit (pkgs.stdenvNoCC.hostPlatform) isDarwin isLinux;
+in
+{
+  age = {
+    identityPaths = [
+      (lib.mkIf isDarwin "/Users/haril/.ssh/id_ed25519_agenix")
+      (lib.mkIf isLinux "/home/haril/.ssh/id_ed25519_agenix")
+    ];
+    secrets = {
+      test-secret = {
+        file = ./secrets/test-secret.age;
+        # path = "/run/secrets/test-secret";
+        # owner = "haril";
+        # group = "staff";
+        # mode = "400";
+      };
+      secret1 = {
+        file = ./secrets/secret1.age;
+      };
+    };
+  };
+}
