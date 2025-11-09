ffcc53591a8cd10e8f7b1b6795ac3b4d31c45739
haril song <songkg7@gmail.com>
2025-11-10 03:39:20 +0900

    wsl linux config

--- modules/linux/configuration.nix
+++ modules/linux/configuration.nix
@@ -1,165 +1,50 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   pkgs,
-  inputs,
   ...
 }:
 {
   imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ]
-  ++ (with inputs.nixos-hardware.nixosModules; [
-    # Include nixos-hardware libraries.
-    common-cpu-amd
-    common-gpu-amd
-    common-pc
-    common-pc-ssd
-  ]);
+  ];
+  wsl.enable = true;
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.loader.grub.configurationLimit = 3;
+  wsl.defaultUser = "haril";
 
-  # Boot output
   boot.kernel.sysctl = {
     "fs.inotify.max_user_watches" = 524288;
   };
-  boot.kernelParams = [
-    "quiet"
-    "rd.udev.log_level=3"
-  ];
-  boot.consoleLogLevel = 0;
-  boot.initrd.verbose = false;
-  boot.plymouth.enable = true;
 
-  # Enable opengl, vdpau
-  hardware.graphics.enable = true;
-
-  environment.variables = {
-    LIBVA_DRIVER_NAME = "radeonsi";
-    VDPAU_DRIVER = "radeonsi";
-  };
-
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Set your time zone.
   time.timeZone = "Asia/Seoul";
 
-  # Network nameservers
   networking = {
+    # networking.hostName = "nixos";
     nameservers = [
       "1.1.1.1"
       "1.0.0.1"
     ];
   };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
   services.pulseaudio.enable = true;
   services.tailscale.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Enable sudo
   security.sudo.enable = true;
-
-  # Enable dconf
   programs.dconf.enable = true;
 
-  # Docker
+  users.users.haril = {
+    shell = pkgs.bashInteractive;
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "docker"
+    ];
+  };
+
   virtualisation.docker = {
     enable = true;
     autoPrune.enable = true;
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.haril = {
-    shell = pkgs.bashInteractive;
-    isNormalUser = true;
-    extraGroups = [
-      "wheel" # Enable ‘sudo’ for the user.
-      "docker"
-    ];
-  };
+  programs.ssh.startAgent = true;
+  programs.nix-ld.enable = true;
 
-  services.getty.autologinUser = "haril";
-
-  environment.loginShellInit = ''
-    if [[ "$(tty)" == "/dev/tty1" ]]; then
-      [ $(command -v sway) ] && sway
-    fi
-  '';
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  #   firefox
-  # ];
-
-  environment.pathsToLink = [
-    "/libexec"
-    "/share/polkit-1"
-    "/share/qemu"
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  programs.ssh = {
-    startAgent = true;
-  };
-
-  programs.nix-ld = {
-    enable = true;
-  };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+  system.stateVersion = "25.05";
 }
