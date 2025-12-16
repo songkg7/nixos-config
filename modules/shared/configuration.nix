07ffba8377d9c3696bedee0213e1e593799ff748
haril song <songkg7@gmail.com>
2025-12-16 10:30:19 +0900

    buffer size scale up

--- modules/shared/configuration.nix
+++ modules/shared/configuration.nix
@@ -8,6 +8,7 @@
       substituters = [
         "https://cache.nixos.org"
       ];
+      download-buffer-size = 524288000;
       # trusted-public-keys = [
       #   "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       # ];
