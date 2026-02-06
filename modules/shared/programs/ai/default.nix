a034ea669a420763c7b18ef2045a7c3f04fa9c43
haril song <songkg7@gmail.com>
2026-02-06 12:27:33 +0900

    update claude-code settings

--- modules/shared/programs/ai/default.nix
+++ modules/shared/programs/ai/default.nix
@@ -60,13 +60,9 @@
 
   programs.claude-code = {
     enable = true;
-    settings = {
-      includeCoAuthoredBy = false;
-      # env = {
-      #   CLAUDE_CODE_IDE_SKIP_AUTO_INSTALL = "1";
-      #   CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = "1";
-      # };
-    };
+    # settings = {
+    #   includeCoAuthoredBy = false;
+    # };
 
     # mcpServers = {
     #   github = {
