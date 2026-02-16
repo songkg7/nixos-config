5b9631c5124837631fde4c7fdb0ef55c9775c1fc
haril song <songkg7@gmail.com>
2026-02-16 23:42:16 +0900

    chore: remove inactive home-manager library modules and empty stubs
    Remove 6 commented-out HM modules (1password, cleanshot, clop, defaults,
    hammerspoon, jq) that were replaced by Homebrew cask management.
    Also remove empty darwin/linux system stubs.

--- libraries/home-manager/default.nix
+++ libraries/home-manager/default.nix
@@ -1,27 +1,5 @@
-{
-  pkgs,
-  lib,
-  ...
-}:
-let
-  inherit (pkgs.stdenvNoCC.hostPlatform) isDarwin isLinux;
-in
-{
+_: {
   home-manager.sharedModules = [
-    # home-manager-secrets (nix flake input)
-    # inputs.home-manager-secrets.homeManagerModules.home-manager-secrets
-
-    # local programs (local)
-    # ./programs/1password
-    # ./programs/cleanshot
-    # ./programs/clop
-    # ./programs/defaults
-    # ./programs/hammerspoon
     ./programs/homerow
-    # ./programs/jq
-
-    # os systems
-    (lib.mkIf isDarwin ./systems/darwin)
-    (lib.mkIf isLinux ./systems/linux)
   ];
 }
