40f59ed2562d68aca7c790309d1d7820c561c26a
haril song <songkg7@gmail.com>
2026-03-13 21:27:16 +0900

    Reapply "update starship config"
    This reverts commit 7e6cd34a251145942f14846274285141c2642fb4.

--- modules/shared/programs/shell/starship/default.nix
+++ modules/shared/programs/shell/starship/default.nix
@@ -1,12 +1,9 @@
-let
-  format = import ./format.nix;
-  prompt = import ./prompt.nix;
-  git = import ./git.nix;
-  languages = import ./languages.nix;
-in
 {
   programs.starship = {
     enable = true;
-    settings = format // prompt // git // languages;
+    presets = [ "pure-preset" ];
+    settings.cmd_duration.format = " [$duration]($style) ";
+    enableZshIntegration = true;
   };
 }
+
