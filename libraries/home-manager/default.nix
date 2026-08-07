087607306c1bb04115d60696000ef2c47fe4652d
haril song <songkg7@gmail.com>
2026-08-07 13:22:15 +0900

    update home-manager config

--- libraries/home-manager/default.nix
+++ libraries/home-manager/default.nix
@@ -1,6 +1,7 @@
-_: {
+{ pkgs, ... }:
+{
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  home-manager.backupFileExtension = "backup";
+  home-manager.backupCommand = "${pkgs.trash-cli}/bin/trash-put";
   home-manager.sharedModules = [ ./shared-user.nix ];
 }
